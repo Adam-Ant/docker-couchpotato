10b2bafa194db26882599d2a83bae8a84a2ff5d3
Joe Groocock <frebib@gmail.com>
2016-09-01 02:30:48 +0100

    Add umask support

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 
 git -C /couchpotato pull
 
+[ -n "$UMASK" ] && umask "$UMASK"
+
 python2.7 /couchpotato/CouchPotato.py --console_log --data_dir=/config/couchpotato --config_file=/config/couchpotato/couchpotato.ini
